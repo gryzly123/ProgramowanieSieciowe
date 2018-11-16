f4f5bf521d3f36b74e914bab71cf70a6761f14ca
Krzysztof Niedźwiecki <Krzysztof Niedwiecki>
2018-11-17 00:54:47 +0100

    uporządkowanie folderu z wypustami binarnymi

---
+++ Binarki/B64/B64_Test.bat
@@ -0,0 +1,4 @@
+b64 input output_raw encode_raw
+b64 input output_pad encode_padded
+b64 output_raw input_afterraw decode
+b64 output_pad input_afterpad decode
--- Binarki/B64_Test.bat
+++
@@ -1,4 +0,0 @@
-b64 input outputr encode_raw
-b64 input outputp encode_padded
-b64 outputr inputr decode
-b64 outputp inputp decode
